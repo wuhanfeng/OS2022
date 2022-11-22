c62b7ab5364704d2e543ca3d8f2f0cfcd93199ff
tracer-nju <tracer@nju.edu.cn>
2022-11-22 16:02:14 +0800

    Linux whf-XPS-8700 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:02:14 up 17 days, 23:45,  1 user,  load average: 0.12, 0.20, 0.13

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -29,7 +29,7 @@ int main(const char *args)
 		}
 		game_progress();
 		screen_update();
-		next_frame += 1000000 / FPS;
+		next_frame += 1000000000 / FPS;
 		printf("uptime() = %d, next_frame = %d\n", uptime(), next_frame);
 	}
 	return 0;
