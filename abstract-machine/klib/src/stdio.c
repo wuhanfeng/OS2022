d45046026de0ef0b6a70bc21d7d8db8c6cde5f4b
wuhanfeng <1101415219@qq.com>
2022-11-23 10:55:31 +0800

    fix x86_64 x86 bugs

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,10 +6,10 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-static char *get_parse_out_d(char *p_out, va_list *ap)
+static char *get_parse_out_d(char *p_out, va_list ap)
 {
 	int d, len = 0, tmp[20];
-	d = va_arg(*ap, int);
+	d = va_arg(ap, int);
 	if (d == 0)
 	{
 		*p_out++ = '0';
@@ -41,10 +41,10 @@ static char *get_parse_out_d(char *p_out, va_list *ap)
 	return p_out;
 }
 
-static char *get_parse_out_x(char *p_out, va_list *ap)
+static char *get_parse_out_x(char *p_out, va_list ap)
 {
 	unsigned int d, len = 0, tmp[20];
-	d = va_arg(*ap, unsigned int);
+	d = va_arg(ap, unsigned int);
 	if (d == 0)
 	{
 		*p_out++ = '0';
@@ -67,11 +67,12 @@ static char *get_parse_out_x(char *p_out, va_list *ap)
 }
 
 
-static char *get_parse_out_p(char *p_out, va_list *ap)
+static char *get_parse_out_p(char *p_out, va_list ap)
 {
 	unsigned int *pd, len = 0, tmp[20];
-	pd = va_arg(*ap, unsigned int*);
-	unsigned int d = (unsigned int)pd;
+	
+	pd = va_arg(ap, unsigned int*);
+	uintptr_t d = (uintptr_t)pd;
 	*p_out++ = '0';
 	*p_out++ = 'x';
 	if (d == 0)
@@ -95,20 +96,20 @@ static char *get_parse_out_p(char *p_out, va_list *ap)
 	return p_out;
 }
 
-static char* get_parse_out_s(char *p_out, va_list* ap)
+static char* get_parse_out_s(char *p_out, va_list ap)
 {
 	char *s;
-	s = va_arg(*ap, char *);
+	s = va_arg(ap, char *);
 	while (*s)
 		*p_out++ = *s++;
 	return p_out;
 }
 
-static char *get_parse_out_ld(char *p_out, va_list *ap)
+static char *get_parse_out_ld(char *p_out, va_list ap)
 {
 	int len = 0, tmp[20];
 	__uint64_t ld;
-	ld = va_arg(*ap, __uint64_t);
+	ld = va_arg(ap, __uint64_t);
 	len = 0;
 	while (ld)
 	{
@@ -132,22 +133,22 @@ static int parse_para(char *out, const char *fmt, va_list ap)
 			switch (*fmt++)
 			{
 			case 's': /* string */
-				p_out = get_parse_out_s(p_out, &ap);
+				p_out = get_parse_out_s(p_out, ap);
 				break;
 			case 'd': /* int */
-				p_out = get_parse_out_d(p_out, &ap);
+				p_out = get_parse_out_d(p_out, ap);
 				break;
 			case 'p':
-				p_out = get_parse_out_p(p_out, &ap);
+				p_out = get_parse_out_p(p_out, ap);
 				break;
 			case 'x':
-				p_out = get_parse_out_x(p_out, &ap);
+				p_out = get_parse_out_x(p_out, ap);
 				break;
 			case 'l':
 				switch (*fmt++)
 				{
 				case 'd':
-					p_out = get_parse_out_ld(p_out, &ap);
+					p_out = get_parse_out_ld(p_out, ap);
 					break;
 				default:
 					break;
