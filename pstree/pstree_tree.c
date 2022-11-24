a557c9053563006d8ff9c52e0c741ca3c134b8a0
tracer-nju <tracer@nju.edu.cn>
2022-11-24 17:21:40 +0800

    Linux whf-XPS-8700 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:21:40 up 20 days,  1:04,  1 user,  load average: 0.12, 0.14, 0.07

--- pstree/pstree_tree.c
+++ pstree/pstree_tree.c
@@ -9,6 +9,7 @@ static PROC* find_proc(pid_t pid)
     PROC* walk;
     for(walk = list; walk; walk = walk->next)
     {
+        LOGD("========3");
         if(walk->node.pid == pid) return walk;
     }
     return NULL;
